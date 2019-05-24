4886423df9c39926906c146fde6ed322865ce877
n3724 <n3724@ndensan.co.jp>
2019-05-24 14:03:29 +0900

    create

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 import numpy as np
+import cv2
+import os
 from autokeras.image.image_supervised import ImageClassifier
 
-x_train = np.load('/opt/data/kuzushiji/kmnist-train-imgs.npz')['arr_0']
-x_test = np.load('/opt/data/kuzushiji/kmnist-test-imgs.npz')['arr_0']
-x_train = x_train.reshape(x_train.shape + (1, ))
-x_test = x_test.reshape(x_test.shape + (1, ))
+x_train = []
+y_train = []
+x_test = []
+y_test = []
 
-y_train = np.load('/opt/data/kuzushiji/kmnist-train-labels.npz')['arr_0']
-y_test = np.load('/opt/data/kuzushiji/kmnist-test-labels.npz')['arr_0']
+
+for file_name in  os.listdir("train/normal"):
+    img = cv2.imread("train/normal/"+file_name)
+    x_train.append(img)
+    # x_train.reshape(256,256,3)
+    y_train.append(0)
+
+for file_name in  os.listdir("train/anomaly"):
+    img = cv2.imread("train/anomaly/"+file_name)
+    x_train.append(img)
+    # x_train.reshape(256,256,3)
+    y_train.append(0)
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+
+for file_name in  os.listdir("test/normal"):
+    img = cv2.imread("test/normal/"+file_name)
+    x_test.append(img)
+    # x_train.reshape(256,256,3)
+    y_test.append(0)
+
+for file_name in  os.listdir("test/anomaly"):
+    img = cv2.imread("test/anomaly/"+file_name)
+    x_test.append(img)
+    # x_train.reshape(256,256,3)
+    y_test.append(0)
+x_test = np.array(x_test)
+y_test = np.array(y_test)
+
+
+print(x_train.shape)
+print(y_train.shape)
 
 clf = ImageClassifier(verbose=True)
 clf.fit(x_train, y_train, time_limit=12 * 60 * 60)
